f7f913aec34ae1f5bd23983bd4258fd925b1d324
NinthStar <ninthstar942653@gmail.com>
2016-08-06 06:26:26 +0000

    [Mod] Input function of component Input and Slider

--- views/components/Input/index.js
+++ views/components/Input/index.js
@@ -7,16 +7,41 @@ import material from '../../public/material/material.scss';
 import style from './style';
 
 export const Input = React.createClass({
+	getInitialState() {
+		const VALUE = this.props.value;
+		
+		return {
+			value: VALUE
+		};
+	},
+	componentWillMount() {
+		const VALUE = this.props.value;
+		
+		this.setState({
+			value: VALUE
+		});
+	},
+	componentWillReceiveProps(nextProps) {
+		this.setState({
+			value: nextProps.value
+		});
+	},
 	handleChange(evt) {
-		this.props.onChange(evt.target.value);
+		this.setState({
+			value: evt.target.value
+		});
+	},
+	handleBlur(evt) {
+		this.props.setValue(this.state.value);
 	},
 	render() {
 		// Get props
-		const {className: CLASSNAME, value: VALUE, onChange, ...PROPS} = this.props;
+		const {className: CLASSNAME, value, setValue, ...PROPS} = this.props;
+		const VALUE = this.state.value;
 		const INPUT_CLASS = classNames([CLASSNAME, style.input]);
 		
 		return (
-			<input className={INPUT_CLASS} value={VALUE} onChange={this.handleChange} {...PROPS}/>
+			<input className={INPUT_CLASS} value={VALUE} onChange={this.handleChange} onBlur={this.handleBlur} {...PROPS}/>
 		);
 	}
 });
